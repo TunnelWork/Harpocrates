f3c1b68712cf99a350ef59fc62488f5f3b6dd982
Gaukas Wang <i@gaukas.wang>
2021-10-14 23:54:24 -0600

    randutil randnumber

--- randutil.go
+++ randutil.go
@@ -3,6 +3,7 @@ package harpocrates
 import (
 	crand "crypto/rand"
 	"math/big"
+	"math/rand"
 )
 
 // Credits:
@@ -18,4 +19,12 @@ func GetRandomString(n uint, runes []rune) (string, error) {
 		b[i] = runes[v.Int64()]
 	}
 	return string(b), nil
+}
+
+func GetRandomNumber(min int, max int) int {
+	c, err := crand.Int(crand.Reader, big.NewInt(int64(max-min)))
+	if err == nil {
+		return int(c.Int64()) + min
+	}
+	return min + rand.Intn(max-min) // or use math/rand instead
 }
